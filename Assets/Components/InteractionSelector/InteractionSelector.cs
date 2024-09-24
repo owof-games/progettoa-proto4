1713a6129aa63cee2e032d4f9d4a4ff5b2946c24
mattia <mattia.belletti@gmail.com>
2024-09-24 20:17:32 +0200

    feat: movements and refactoring.

--- Assets/Components/InteractionSelector/InteractionSelector.cs
+++ Assets/Components/InteractionSelector/InteractionSelector.cs
@@ -1,16 +1,51 @@
+using NUnit.Framework;
+using UnityAtoms.BaseAtoms;
 using UnityEngine;
 
-public class InteractionSelector : MonoBehaviour
+namespace Components.InteractionSelector
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    enum Interaction
     {
-        
+        Exit
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    public class InteractionSelector : MonoBehaviour
     {
-        
+        [SerializeField] private Animator animator;
+        [SerializeField] private Interaction interaction;
+        [SerializeField] private string interactionKey;
+        [SerializeField] private StringEvent interactionExitEvent;
+        private int _hoveredHash = -1;
+
+        private void Awake()
+        {
+            _hoveredHash = Animator.StringToHash("Hovered");
+            Assert.IsNotNull(animator);
+            Assert.IsNotNull(interactionExitEvent);
+            Assert.IsFalse(string.IsNullOrWhiteSpace(interactionKey));
+        }
+
+        private void OnMouseEnter()
+        {
+            animator.SetBool(_hoveredHash, true);
+        }
+
+        private void OnMouseExit()
+        {
+            animator.SetBool(_hoveredHash, false);
+        }
+
+        private void OnMouseUp()
+        {
+            switch (interaction)
+            {
+                case Interaction.Exit:
+                    interactionExitEvent.Raise(interactionKey);
+                    break;
+                default:
+                    Debug.LogError($"Unknown interaction {interaction}");
+                    break;
+            }
+        }
     }
 }

c869970fd2ae6614bd27ff45b5cde7d521f83bb4
mattia <mattia.belletti@gmail.com>
2024-11-16 14:15:42 +0100

    feat: drop inventory object.

--- Assets/Components/Inventory/InventoryObject.cs
+++ Assets/Components/Inventory/InventoryObject.cs
@@ -2,25 +2,53 @@ using System.Linq;
 using Components.Configuration.InanimateObjects;
 using Components.InteractionSelector;
 using LemuRivolta.InkAtoms;
+using UnityAtoms.BaseAtoms;
 using UnityEngine;
 using UnityEngine.Assertions;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace Components.Inventory
 {
-    public class InventoryObject : MonoBehaviour
+    public class InventoryObject : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
         [SerializeField] private InanimateObjectDescriptions inanimateObjectDescriptions;
-        [SerializeField] private Color interactableColor = Color.white;
-        [SerializeField] private Color nonInteractableColor = new(1, 1, 1, 0);
+        [SerializeField] private GameObject dropIcon;
+        [SerializeField] private StringEvent dropObjectEvent;
+        private Button _button;
 
         private string _currentName;
         private Image _image;
+
+        private bool _inside;
+
+        private bool _interactable;
 
         private void Start()
         {
             _image = GetComponent<Image>();
             Assert.IsNotNull(_image);
+
+            _button = GetComponent<Button>();
+            Assert.IsNotNull(_button);
+
+            Assert.IsNotNull(dropIcon);
+
+            Assert.IsNotNull(inanimateObjectDescriptions);
+
+            Assert.IsNotNull(dropObjectEvent);
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            _inside = true;
+            UpdateDropIcon();
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            _inside = false;
+            UpdateDropIcon();
         }
 
         public void OnInventoryObjectAdded(SerializableInkListItem obj)
@@ -34,13 +62,25 @@ namespace Components.Inventory
         {
             _currentName = null;
             _image.enabled = false;
+            _button.interactable = false;
         }
 
         public void OnAvailableInteractionsChanged(AvailableInteractions availableInteractions)
         {
-            var isInteractable = availableInteractions.availableInteractions.Any(i =>
+            _interactable = availableInteractions.availableInteractions.Any(i =>
                 i.interaction == Interaction.DropObject && i.key == _currentName);
-            _image.color = isInteractable ? interactableColor : nonInteractableColor;
+            _button.interactable = _interactable;
+            UpdateDropIcon();
+        }
+
+        private void UpdateDropIcon()
+        {
+            dropIcon.SetActive(_inside && _interactable);
+        }
+
+        public void OnClick()
+        {
+            dropObjectEvent.Raise(_currentName);
         }
     }
 }

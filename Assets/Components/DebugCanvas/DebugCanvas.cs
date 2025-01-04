a966f6d4740019ef37da8c2a86c071080e281350
mattia <mattia.belletti@gmail.com>
2025-01-04 21:44:38 +0100

    feat: initial debug screen.

--- Assets/Components/DebugCanvas/DebugCanvas.cs
+++ Assets/Components/DebugCanvas/DebugCanvas.cs
@@ -1,24 +1,95 @@
+using System;
+using Cysharp.Threading.Tasks;
+using LemuRivolta.InkAtoms;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 namespace Components.DebugCanvas
 {
     public class DebugCanvas : MonoBehaviour
     {
         [SerializeField] private GameObject container;
+        [SerializeField] private TextMeshProUGUI gameNameTextMeshPro;
         [SerializeField] private TextMeshProUGUI versionTextMeshPro;
+        [SerializeField] private TextMeshProUGUI sessionGuidTextMeshPro;
+        [SerializeField] private Button copySessionGuidButton;
+        [SerializeField] private TextMeshProUGUI copySessionGuidTextMeshProUGUI;
+
+        private string _originalCopySessionGuidText;
 
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         private void Start()
         {
             container.SetActive(false);
+            copySessionGuidButton.onClick.AddListener(OnCopySessionGuidClicked);
+            _originalCopySessionGuidText = copySessionGuidTextMeshProUGUI.text;
+        }
+
+        private void OnCopySessionGuidClicked()
+        {
+            GUIUtility.systemCopyBuffer = RemoteLogger.RemoteLogger.SessionGuid;
+            copySessionGuidTextMeshProUGUI.text = "session guid copied";
         }
 
         public void OnDebugToggle(InputAction.CallbackContext _)
         {
-            container.SetActive(!container.activeSelf);
-            versionTextMeshPro.text = $"Version: {Application.version}";
+            if (container.activeSelf)
+            {
+                container.SetActive(false);
+            }
+            else
+            {
+                container.SetActive(true);
+                gameNameTextMeshPro.text = "Game name: " + RemoteLogger.RemoteLogger.GameName;
+                versionTextMeshPro.text = $"Version: {Application.version}";
+                sessionGuidTextMeshPro.text = "Session GUID: " + RemoteLogger.RemoteLogger.SessionGuid;
+                copySessionGuidButton.GetComponentInChildren<TextMeshProUGUI>().text = _originalCopySessionGuidText;
+            }
+        }
+
+        public void OnContinue(string flowName)
+        {
+            OnContinueAsync(flowName).Forget();
+        }
+
+        private static async UniTaskVoid OnContinueAsync(string flowName)
+        {
+            try
+            {
+                await RemoteLogger.RemoteLogger.Log($"Continue on flow {flowName}", new[]
+                {
+                    ("operation", "continue"),
+                    ("flowName", flowName)
+                });
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e);
+            }
+        }
+
+        public void OnChoice(ChosenChoice choice)
+        {
+            OnChoiceAsync(choice).Forget();
+        }
+
+        private static async UniTaskVoid OnChoiceAsync(ChosenChoice choice)
+        {
+            try
+            {
+                await RemoteLogger.RemoteLogger.Log($"Take choice {choice.ChoiceIndex} on flow {choice.FlowName}", new[]
+                {
+                    ("operation", "choice"),
+                    ("flowName", choice.FlowName),
+                    ("choiceIndex", choice.ChoiceIndex.ToString())
+                });
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e);
+            }
         }
     }
 }
---
+++ Assets/Components/RemoteLogger/RemoteLogger.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Components.RemoteLogger
+{
+    public static class RemoteLogger
+    {
+        public const string GameName = "Loop Proto 4";
+        private static string _sessionGuid;
+
+        public static string SessionGuid
+        {
+            get
+            {
+                _sessionGuid ??= Guid.NewGuid().ToString();
+                return _sessionGuid;
+            }
+        }
+
+        public static async ValueTask Log(string message, IEnumerable<(string, string)> metadata)
+        {
+            var sessionGuid = SessionGuid;
+
+            var formData = new List<IMultipartFormSection>
+            {
+                new MultipartFormDataSection("message", message),
+                new MultipartFormDataSection("gameName", GameName),
+                new MultipartFormDataSection("version", Application.version),
+                new MultipartFormDataSection("sessionGuid", sessionGuid)
+            };
+            formData.AddRange(metadata.Select(entry =>
+                new MultipartFormDataSection(entry.Item1,
+                    string.IsNullOrEmpty(entry.Item2) ? "<<<empty>>>" : entry.Item2)));
+
+            await UniTask.SwitchToMainThread();
+            var unityWebRequest = UnityWebRequest.Post("https://game-logger.foxthesystem.space", formData);
+            // var unityWebRequest = UnityWebRequest.Post("http://localhost:9283", formData);
+            await unityWebRequest.SendWebRequest();
+            if (unityWebRequest.result != UnityWebRequest.Result.Success)
+                throw new Exception(
+                    $"Web request to record logs failed: {unityWebRequest.error} - {unityWebRequest.downloadHandler.text}");
+        }
+    }
+}
